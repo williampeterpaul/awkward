05ba6c02be0c9915a87bc21dae5703a0bbb136ce
williampeterpaul <william.p.paul@gmail.com>
2018-05-07 13:31:38 +0100

    Refactor pages

--- awkward.ui/Pages/Index.cshtml.cs
+++ awkward.ui/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using awkward.api.Models;
+using awkward.ui.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,41 @@ namespace awkward.ui.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IndexModel(IApiClient client)
         {
+            Client = client;
+        }
 
+        [BindProperty]
+        public Entity EntityA { get; set; }
+
+        [BindProperty]
+        public Entity EntityB { get; set; }
+
+        private IApiClient Client { get; }
+
+        public async Task OnGetAsync()
+        {
+            EntityA = await Client.GetEntityAsync(new Random().Next(3, 100));
+            EntityB = await Client.GetEntityAsync(new Random().Next(3, 100));
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var exchange = Elo.PointsExchanged(EntityA.Rating, EntityB.Rating);
+
+            EntityA.Rating += exchange;
+            EntityB.Rating -= exchange;
+
+            await Client.PutEntityAsync(EntityA);
+            await Client.PutEntityAsync(EntityB);
+
+            return Page();
         }
     }
 }
