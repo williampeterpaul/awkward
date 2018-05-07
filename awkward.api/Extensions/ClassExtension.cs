ec90d248141fb73d3315a47ab23019f8ec360924
williampeterpaul <william.p.paul@gmail.com>
2018-05-07 02:49:41 +0100

    Add extension methods for enum and class

---
+++ awkward.api/Extensions/ClassExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awkward.api.Extensions
+{
+    public static class ClassExtension
+    {
+        public static string PropertyList(this object obj)
+        {
+            var properties = obj.GetType().GetProperties();
+
+            var builder = new StringBuilder();
+
+            foreach (var property in properties)
+            {
+                builder.AppendLine(property.Name + ": " + property.GetValue(obj, null));
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ awkward.api/Extensions/EnumExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace awkward.api.Extensions
+{
+    public static class EnumExtension
+    {
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static object TryParseDefault<T>(this string valueToParse, bool ignoreCase = true)
+        {
+            object result = default(T);
+
+            if (Enum.IsDefined(typeof(T), valueToParse))
+            {
+                result = Enum.Parse(typeof(T), valueToParse, ignoreCase);
+            }
+
+            return result;
+        }
+
+        public static object TryParseDefault<T>(this int valueToParse)
+        {
+            var result = Enum.GetName(typeof(T), valueToParse);
+
+            return result.TryParseDefault<T>();
+        }
+    }
+}
