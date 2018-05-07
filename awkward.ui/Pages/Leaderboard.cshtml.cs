3d76062538543473882cd5e662e8e7a9e035caba
williampeterpaul <william.p.paul@gmail.com>
2018-05-07 13:31:10 +0100

    Add faceted leaderboard concept

--- awkward.ui/Pages/Leaderboard.cshtml.cs
+++ awkward.ui/Pages/Leaderboard.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using awkward.api.Models;
 using awkward.ui.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static awkward.api.Models.Enumerations;
 
 namespace awkward.ui.Pages
 {
@@ -18,6 +20,15 @@ namespace awkward.ui.Pages
 
         public IList<Entity> Entities { get; set; }
 
+        [BindProperty]
+        public Grade Grade { get; set; }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        [BindProperty]
+        public Medium Medium { get; set; }
+
         private IApiClient Client { get; }
 
         public async Task OnGetAsync()
@@ -25,6 +36,13 @@ namespace awkward.ui.Pages
             Entities = await Client.GetEntitiesAsync();
 
             Entities = Entities.OrderByDescending(e => e.Rating).Take(20).ToList();
+        }
+
+        public async Task OnPostAsync()
+        {
+            Entities = await Client.GetEntitiesAsync();
+
+            Entities = Entities.OrderByDescending(e => e.Rating).Where(e => e.Category.Equals(Category) && e.Medium.Equals(Medium) && e.Grade.Equals(Grade)).ToList();
         }
     }
 }
